67372a31df8d21ed52aeb2d410f631b8dd374370
Foggy1983 <79501078+Foggy1983@users.noreply.github.com>
2021-02-28 16:16:37 +0400

    Create git_sync-v1.py

---
+++ git_sync-v1.py
@@ -0,0 +1,29 @@
+from datetime import timedelta, datetime
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2021, 2, 28, 7, 55, 0),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=50),
+}
+
+dag = DAG(
+    'git_sync1',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(seconds=60),
+)
+
+git_pull = BashOperator(
+    task_id='git_pull',
+    bash_command='git -C /root/airflow/dags/ pull',
+    dag=dag,
+)
